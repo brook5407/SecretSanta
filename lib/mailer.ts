7ead333ee2ab0599779e7a5f94d2f90a4cb2e579
brookchin <brookchin@twm.com.sg>
2024-12-04 10:00:48 +0800

    chore: change email message

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -16,7 +16,7 @@ export const sendEmail = async (
     recipient: string
 ) => {
     await transporter.sendMail({
-      from: `"TWM Committee Team 2024" <${process.env.MAIL_USER}>`,
+      from: `<${process.env.MAIL_USER}>`,
       to: email,
       subject: "Your Secret Santa Match Is Here!",
       html: `
@@ -26,18 +26,11 @@ export const sendEmail = async (
         
         <p>🎁 Your Secret Santa match is: <strong>${recipient}</strong></p>
         
-        <p>Please keep it a secret and make sure to bring something thoughtful and fun to make their day! Here are a few details to help you plan your gift:</p>
-        
-        <ul style="list-style-type: none; padding: 0;">
-            <li><strong>Gift exchange date:</strong> 28 Nov 2024</li>
-            <li><strong>Gift budget:</strong> $20</li>
-        </ul>
-
-        <p>Please mention the name of the person you are gifting on the gift and leave it in the meeting room when you ready.</p>
+        <p>Please keep it a secret and make sure to bring something thoughtful and fun to make their day!</p>
         
         <p>If you have any questions or need more details, feel free to reach out to us. Thanks for being a part of the fun, and happy gifting!</p>
         
-        <p>Best wishes,<br><strong>TWM Commitee Team 2024 🎅</strong></p>
+        <p>Best wishes,<br><strong>${process.env.MAIL_USERNAME} 🎅</strong></p>
 
         <p>This is an auto-generated email. Please do not reply to this message.</p>
       `,
